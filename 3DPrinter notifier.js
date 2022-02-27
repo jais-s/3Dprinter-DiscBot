77a50eb7fa1190e3c3a3805855387ac9fbdbae64
jais-s <Gtmv2002@gmail.com>
2022-02-26 22:23:12 -0700

    pps

--- 3DPrinter notifier.js
+++ 3DPrinter notifier.js
@@ -1,14 +1,25 @@
-const Discord =require('discord.js');
+/*const Discord =require('discord.js');
 require('dotenv').config();
 const client =new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-const channel =client.guilds.cache.get('id')
-
+const channel =client.guilds.cache.get('id');
+var OctoprinterStatusPrint = require('octo-client');
 client.on('message',(msg)=> {
     console.log("3dprinter updater bot online")
     if(msg.content=='!hello'){
         msg.reply ('yo')
     }
+    if(msg.content=='!3dprintStat'){
+        msg.reply (printerStatus.toString())
+    }
 });
 
-
+OctoprinterStatusPrint.printerState(function(response){
+   console.log(response);  });
 client.login(process.env.DISCORD_TOKEN);
+*/
+var OctoPrint = require('octo-client');
+
+OctoPrint.printerState(function(response){
+  console.log(response);
+  
+});
